186a5bb0ba5ba702bd596207cb0e98571db87928
David Flavigné <david.flavigne@gmail.com>
2020-08-22 18:09:32 +0200

    #4 Ajout des icones

--- src/dbus-notifications.js
+++ src/dbus-notifications.js
@@ -1,4 +1,6 @@
-import Dbus from 'dbus'
+import Dbus from 'dbus';
+import Path from 'path';
+import Fs from 'fs';
 
 export default class Notifications {
     busType=process.env.DBUS_TYPE;
@@ -7,6 +9,8 @@ export default class Notifications {
     busInterface=process.env.DBUS_INTERFACE;
     busMethod=process.env.DBUS_NOTIFY_METHOD;
     iface = null;
+    iconPath=Path.resolve()+'/resources/icons';
+    defaultIconName=Path.resolve()+'/resources/icons/notif_64x64.png';
 
     constructor(conn){
         let self = this;
@@ -24,6 +28,13 @@ export default class Notifications {
     }
 
     notify(data){
-        if(this.iface) this.iface[this.busMethod]("Gotify", 0, "", data.title, data.message ,[],[],0 );
+        let iconName = this.iconPath+"/"+data.title+"_64x64.png";
+        let self=this;
+        Fs.exists(iconName,exists=>{
+            let icon=self.defaultIconName;
+            if(exists) icon=iconName;
+            
+            if(self.iface) self.iface[self.busMethod]("Gotify", 0, icon, data.title, data.message ,[],[],0 );
+        })
     }
 }
